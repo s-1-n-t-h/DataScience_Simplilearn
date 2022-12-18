83f0c8d9ea6aab041e8688018f7a4a7ba4dcd5bd
YASWANTH BATTU <83590022+s-1-n-t-h@users.noreply.github.com>
2022-12-18 13:27:00 +0530

    removed datasets not needed and added ibm dataset

--- datasets/seaborn-data-master/process/dowjones.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-
-if __name__ == "__main__":
-
-    (
-        pd.read_csv("raw/dowjones.csv")
-        .set_axis(["Date", "Price"], axis=1)
-        .to_csv("dowjones.csv", index=False)
-    )
